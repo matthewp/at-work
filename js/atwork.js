7d933284b5c4d8790c9ae6c718293f2005b189c1
Matthew Phillips <matthew@matthewphillips.info>
2012-01-24 07:55:58 -0500

    Retabbing atwork.js

--- js/atwork.js
+++ js/atwork.js
@@ -5,81 +5,81 @@
 'use strict';
 
 function TimeSpan(ms) {
-	if(ms) {
-		this.totalmilliseconds = ms;
-		this.seconds = Math.floor(ms/1000) % 60;
-		this.minutes = Math.floor(ms/60000) % 60;
-		this.hours = Math.floor(ms/3600000) % 24;
-	}
+  if(ms) {
+    this.totalmilliseconds = ms;
+    this.seconds = Math.floor(ms/1000) % 60;
+    this.minutes = Math.floor(ms/60000) % 60;
+    this.hours = Math.floor(ms/3600000) % 24;
+  }
 }
 
 TimeSpan.prototype = {
-	totalmilliseconds: 0,
-	seconds: 0,
-	minutes: 0,
-	hours: 0,
-	add: function(other) {
-		other = typeof other === "number" ? other : other.totalmilliseconds;
-		var ms = this.totalmilliseconds + other;
-		return new TimeSpan(ms);
-	},
-	toString: function() {
-		var str = "", self = this;
-		[this.hours, this.minutes, this.seconds].forEach(function(num) {
-			str += self._zeroFill(num,2) + ":";
-		});
-		return str.substr(0,str.length - 1);
-	},
-	_zeroFill: function(num,width) {
-		width -= num.toString().length;
-		if(width > 0)
-			return new Array(width + (/\./.test(num) ? 2 : 1)).join('0') + num;
-		
-		return num;
-	}
+  totalmilliseconds: 0,
+  seconds: 0,
+  minutes: 0,
+  hours: 0,
+  add: function(other) {
+    other = typeof other === "number" ? other : other.totalmilliseconds;
+    var ms = this.totalmilliseconds + other;
+    return new TimeSpan(ms);
+  },
+  toString: function() {
+    var str = "", self = this;
+    [this.hours, this.minutes, this.seconds].forEach(function(num) {
+      str += self._zeroFill(num,2) + ":";
+    });
+    return str.substr(0,str.length - 1);
+  },
+  _zeroFill: function(num,width) {
+    width -= num.toString().length;
+    if(width > 0)
+      return new Array(width + (/\./.test(num) ? 2 : 1)).join('0') + num;
+    
+    return num;
+  }
 
 };
 
 function Timer() {
-	this.time = new TimeSpan();
-	this._elem = document.getElementById('current-time');
-	this._btn = document.getElementsByName('start')[0];
+  this.time = new TimeSpan();
+  this._elem = document.getElementById('current-time');
+  this._btn = document.getElementsByName('start')[0];
 
   var self = this;
   [ 'touchstart', 'touchend', 'mousedown', 'mouseup' ].forEach(function(evt) {
-	  self._btn.addEventListener(evt, self);
+    self._btn.addEventListener(evt, self);
   });
 }
 
 Timer.prototype = {
-	running: false,
-	start: function() {
-		this.running = true;
-		this._begin = new Date();
-		this._id = setInterval(this.update.bind(this), 500);
-	
-		this.setBtnText("Stop");
+  running: false,
+  start: function() {
+    this.running = true;
+    this._begin = new Date();
+    this._id = setInterval(this.update.bind(this), 500);
+  
+    this.setBtnText("Stop");
     this._btn.className = 'started';
-	},
-	stop: function() {
-		this.running = false;
-		clearInterval(this._id);
-		this._id = null;
+  },
+  stop: function() {
+    this.running = false;
+    clearInterval(this._id);
+    this._id = null;
 
-		this.time = this.elapsed;
+    this.time = this.elapsed;
 
-		this.setBtnText("Start");
+    this.setBtnText("Start");
     this._btn.className = null;
-	},
+  },
   complete: function() {
     // TODO Save this time to the database.
   },
-	update: function() {
-		var ts = this.elapsed;
-		this._elem.innerHTML = ts.toString();
+  update: function() {
+    var ts = this.elapsed;
+    this._elem.innerHTML = ts.toString();
     this.saveState(ts);
-	},
-	handleEvent: function timerHandle(e) {
+  },
+  handleEvent: function timerHandle(e) {
     switch(e.type) {
       case 'touchstart':
       case 'mousedown':
@@ -91,19 +91,19 @@ Timer.prototype = {
         break;
     }
 
-		e.preventDefault();
-	},
-	setBtnText: function(text) {
-		this._btn.textContent = text;
-	},
-	get elapsed() {
-		if(!this._begin)
-			return NaN;
+    e.preventDefault();
+  },
+  setBtnText: function(text) {
+    this._btn.textContent = text;
+  },
+  get elapsed() {
+    if(!this._begin)
+      return NaN;
 
-		var now = new Date();
-		var ms = now - this._begin;
-		return this.time.add(ms);
-	},
+    var now = new Date();
+    var ms = now - this._begin;
+    return this.time.add(ms);
+  },
   saveState: function(ts) {
     localStorage['enabled'] = this.running;
     var strTime = JSON.stringify(ts);
@@ -112,7 +112,7 @@ Timer.prototype = {
 };
 
 Timer.init = function() {
-	this.timer = new Timer();
+  this.timer = new Timer();
 
   if(localStorage['enabled'] === 'true') {
     this.restore();
@@ -130,8 +130,8 @@ Timer.restore = function() {
 };
 
 window.addEventListener('load', function winLoad(e) {
-	window.removeEventListener('load', winLoad);
-	Timer.init();
+  window.removeEventListener('load', winLoad);
+  Timer.init();
 });
 
 })();
