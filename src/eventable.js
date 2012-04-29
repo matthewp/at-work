c9a671ed79d3b90e90303b8bf5691b4506d65e84
Matthew Phillips <phillipsm2@gmail.com>
2012-04-28 20:02:40 -0400

    Added Eventable object that acts as a base for objects wanting events.

--- src/eventable.js
+++ src/eventable.js
@@ -1,1 +1,33 @@
+function Eventable() {
+  this.events = new Dict();
+}
 
+Eventable.prototype = {
+
+  on: function(evt, action) {
+    var items = this.events.get(evt);
+
+    if(!items) {
+      items = [];
+
+      this.events.set(evt, items);
+    }
+
+    items.push(action);
+  },
+
+  trigger: function(evt) {
+    var items = this.events.get(evt) || [];
+
+    items.forEach(function(action) {
+      action();
+    });
+  },
+
+  dispose: function() {
+    this.trigger('dispose');
+
+    this.events.del('dispose');
+  }
+
+};
